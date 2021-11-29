d3e94a0244dd38bc7d740deab36f2d30a4b5ca2b
Shane <dunnser97@gmail.com>
2021-11-29 13:11:06 +0000

    Debug

--- zcc/settings.py
+++ zcc/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = logins.django_key["new"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
