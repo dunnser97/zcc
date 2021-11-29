e53a08227da23c9a6454dadddaae5ecef8b2a506
Shane <dunnser97@gmail.com>
2021-11-29 05:40:41 +0000

    Reassigned lookup for templates to reflect current structure of project

--- zcc/settings.py
+++ zcc/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'zcc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'masterOfTickets/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
