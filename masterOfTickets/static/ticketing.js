1cd560386f564999ff7d41f142412f277c9152bd
Shane <dunnser97@gmail.com>
2021-11-29 07:15:47 +0000

    init.py

--- masterOfTickets/static/ticketing.js
+++ masterOfTickets/static/ticketing.js
@@ -1,12 +1,17 @@
-fetch("/showMeLists").then(response => {
-    return response.json();
-}).then(data => {
-    console.log(data)
-    results_output = "<table><tr><th>Subject</th><th>Requester</th><th>Requested</th><th>priority</th></tr>"
-
-    for (var key in data["tickets"]) {
-        results_output += "<tr><td>"+data["tickets"][key]['subject']+"</td><td>"+data["tickets"][key]['submitter_id']+"</td><td>"+data["tickets"][key]['created_at']+"</td><td>"+data["tickets"][key]['priority']+"</td></tr>"
-    }
-    results_output += "</table>"
-    document.getElementById("searchresults").innerHTML = results_output;
-})
+function pagenum(page) {
+    fetch("/showMeLists").then(response => {
+        return response.json();
+    }).then(data => {
+        console.log(data)
+        var key = 0 + page*25;
+        results_output = "<table><tr><th>Subject</th><th>Requester</th><th>Requested</th><th>priority</th></tr>"
+        while (key < 25 + page*25) {
+            results_output += "<tr><td>" + data["tickets"][key]['subject'] + "</td><td>" + data["tickets"][key]['submitter_id'] + "</td><td>" + data["tickets"][key]['created_at'] + "</td><td>" + data["tickets"][key]['priority'] + "</td></tr>"
+            key += 1;
+        }
+        results_output += "</table>"
+        document.getElementById("searchresults").innerHTML = results_output;
+        page_nums = Math.ceil(data["tickets"].length/25)
+        console.log(page_nums)
+    })
+}
--- masterOfTickets/views.py
+++ masterOfTickets/views.py
@@ -5,7 +5,12 @@ from masterOfTickets import logins
 import json
 
 def home(request):
-    return render(request, 'home.html')
+    response = requests.get("https://zccrivia.zendesk.com/api/v2/tickets.json",
+                            auth=(logins.zcc["username"], logins.zcc["password"]))
+    if response.status_code != 200:
+        HttpResponse("It appears the API is unavailable or something has gone horribly wrong...")
+    else:
+        return render(request, 'home.html')
 
 def showMeLists(request):
     """
